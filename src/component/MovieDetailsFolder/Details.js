c15fb3a0296e1301a6cc51143359207be965887b
Suyog Patil <suyog3198@gmail.com>
2021-08-17 17:52:03 +0530

    changes made to Details

--- src/component/MovieDetailsFolder/Details.js
+++ src/component/MovieDetailsFolder/Details.js
@@ -7,18 +7,16 @@ const genres = [{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,
 const Details = () => {
     const {movieDetails} = useContext(GlobalContext);
     const[castMembers,setCastMembers] = useState([]);
-    const[trailerLink,setTrailerLink] = useState("")
+    const[trailerLink,setTrailerLink] = useState("");
     const alert = useAlert();
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/movie/${movieDetails.id}/credits?api_key=9bacece0951133e3bcf8bccf45202444&language=en-US`)
         .then(res => res.json())
         .then((data) => {
             if(!data.errors){
-                console.log("Cast members",data.cast);
                 setCastMembers(data.cast);
             }
-            else{
-                setCastMembers([]);
+            else{setCastMembers([]);
             }
         })
         if(movieDetails!==[]){
@@ -27,45 +25,35 @@ const Details = () => {
             .then((data) => {
                 if(!data.errors){
                     let obj = data.results.find((el) => (el.type ==="Trailer" && el.site === "YouTube"))
-                    console.log(obj.key);
-                    (obj !== undefined) ? setTrailerLink(obj.key) : setTrailerLink("");
+                    (obj !== undefined) && setTrailerLink(obj.key);
                 }
-                else{
-                    setTrailerLink("");
+                else{setTrailerLink("");
                 }
             })
             .catch(error => {
                 return null;
             })
         }
-      
     }, [movieDetails])
     const{addMovieToWatched,addToWatchList,watchList,watched} = useContext(GlobalContext);
-    let storedWatchListMovie = watchList.find(m => m.id === movieDetails.id);
+    const storedWatchListMovie = watchList.find(m => m.id === movieDetails.id);
     const storedWatchedMovie = watched.find( m => m.id === movieDetails.id);
-    console.log("Id is",movieDetails.id);
-    const disabledWatchlistButton = storedWatchListMovie 
-                                    ? true 
-                                    : storedWatchedMovie
-                                    ? true 
-                                    : false;
-    const disabledWatchedButton = storedWatchedMovie ? true : false
-    let genre=[];
+    const disabledWatchlistButton = !!(storedWatchListMovie || storedWatchedMovie)
+    const disabledWatchedButton = !!(storedWatchedMovie)
     return (
-        <div> {(movieDetails!==undefined && movieDetails.genre_ids!==undefined) ? 
+        <div> {(movieDetails&& movieDetails.genre_ids) ? 
         <div style={{backgroundImage:`url("https://image.tmdb.org/t/p/w500${movieDetails.backdrop_path}")`,width:'100%',backgroundRepeat:'no-repeat',backgroundSize:'100%',height:'100%'}}>
              <div className="detailsContent row">
                 <div className="col-xs-4 col-sm-6 col-md-5 col-lg-4">
                     <img className="image" alt="image-error"src={`https://image.tmdb.org/t/p/w300${movieDetails.poster_path}`}/>
                 </div>
                 <div className="col-xs-8 col-sm-6 col-md-7 col-lg-8">
-                    <h2 className="detailsTitle">
-                        {movieDetails.title}
-                    </h2>
-                    <span> Released: {movieDetails.release_date.toLocaleString()}</span> <span className="genreWrapper"> 
+                    <h2 className="detailsTitle">{movieDetails.title}</h2>
+                    <span> Release Date: {movieDetails.release_date.toLocaleString()}</span> 
+                    <span className="genreWrapper"> 
                     {
-                    movieDetails.genre_ids.map(element => {
-                                return genres.map(el => (el.id == element) ? (<span className="genre">{el.name}</span>):"")
+                        movieDetails.genre_ids.map(element => {
+                            return genres.map(el => (el.id == element) && <span className="genre">{el.name}</span>)
                         })
                     }
                     </span>
@@ -85,8 +73,7 @@ const Details = () => {
                                                 }
                                         }>
                                 {disabledWatchlistButton ?
-                                <i class="fas fa-check"></i> : 
-                                <i class="fas fa-plus"></i>} 
+                                <i class="fas fa-check"></i> : <i class="fas fa-plus"></i>} 
                                 Watchlist
                             </button>
                         </span>
@@ -98,16 +85,15 @@ const Details = () => {
                                         }
                                     }>
                                 {disabledWatchedButton ? 
-                                <i class="fas fa-check"></i> : 
-                                <i class="fas fa-plus"></i>} 
-                                Watched</button>
+                                <i class="fas fa-check"></i> : <i class="fas fa-plus"></i>} 
+                                Watched
+                            </button>
                         </span>
                         {
-                            (trailerLink!=="") ? 
+                            (trailerLink!=="") && 
                             <span >
-                                <button className="addButton" onClick={()=> window.open(`https://www.youtube.com/watch?v=${trailerLink}`, "_blank")}><img src="https://img.icons8.com/color/24/ffffff/youtube-play.png"/> Trailer</button>
+                                <button className="addButton"><a href={`https://www.youtube.com/watch?v=${trailerLink}`} target="_blank" className="trailerLink"><img src="https://img.icons8.com/color/24/ffffff/youtube-play.png"/> Trailer</a></button>
                             </span>
-                            : ""
                         }
                     </span>
                     <div>
@@ -121,5 +107,4 @@ const Details = () => {
         </div>
     )
 }
-
 export default Details
